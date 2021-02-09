05634da4cd8fef90c82cee7dd3053ebcbee782df
dzakimubarok <fauziyezaki@gmail.com>
2021-02-09 16:41:37 +0700

    Minggu 1 hari 2

---
+++ novice/01-02/kasus/main5.js
@@ -0,0 +1,14 @@
+for (var x=0; x<=15; x++) {
+  if (x === 0) {
+          document.write(x + "is even <br>");
+          console.log(x +  " is even");
+  }
+  else if (x % 2 == 0) {
+          document.write(x + "is even <br>");
+          console.log(x + " is even");   
+  }
+  else {
+          document.write(x + "is odd <br>");
+          console.log(x + " is odd");
+  }
+}
---
+++ novice/01-02/kasus/main9.js
@@ -0,0 +1,19 @@
+function three_digit_armstrong_number() 
+{
+ for (let i = 1; i < 10; ++i) 
+ {
+   for (let j = 0; j < 10; ++j) 
+     {
+        for (let k = 0; k < 10; ++k)
+        {
+          let pow = (Math.pow(i,3) + Math.pow(j,3) + Math.pow(k,3));
+          let plus = (i * 100 + j * 10 +  k);
+          if (pow == plus) 
+           {     
+             console.log(pow);
+            }
+         }
+       }
+    }
+  }
+three_digit_armstrong_number();
